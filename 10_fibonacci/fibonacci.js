3c90c232a5d94432fc1ffbcd876bd9efbdb19b3f
Josephdada01 <ridwandada500@gmail.com>
2023-12-29 03:38:42 +0100

    task continyation

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number <= 1) {
+        return number;
+    }
+    let previous = 0;
+    let current = 1;
 
+    for (let i = 2; i <= number; i++){
+        const next = previous + current;
+        previous = current;
+        current = next;
+    }
+    return current;
 };
 
 // Do not edit below this line
